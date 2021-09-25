ee5ac0e96ab57dd6bc4e3633929d8e950f5ad75a
bsh6463 <bsh9510@gmail.com>
2021-09-25 16:27:21 +0900

    210925 Bean Validation, DTO

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,22 +9,21 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "10000원 넘게 입력해")
 public class Item {
 
 
-    @NotNull(groups = UpdateCheck.class)
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max=1000000)
+   // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min=1000, max=1000000)
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
+   // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  //  @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
